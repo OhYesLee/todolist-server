0ab587cd1a824992cf553abb7d4d79991bc203cd
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-10-20 23:07:58 +0900

    20221020

--- src/main/java/org/example/repository/TodoRepository.java
+++ src/main/java/org/example/repository/TodoRepository.java
@@ -1,6 +1,6 @@
-package repository;
+package org.example.repository;
 
-import model.TodoEntity;
+import org.example.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
